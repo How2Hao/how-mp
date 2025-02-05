7b9cc7aeef1061c6bb9c34680529b5da02f4f831
FliPPeDround <734243792@qq.com>
2025-02-05 11:50:18 +0800

    chore: eslint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
-import { createSSRApp } from 'vue'
-import 'core-js/actual/array/iterator';
-import 'core-js/actual/promise';
-import 'core-js/actual/object/assign';
-import 'core-js/actual/promise/finally';
-import 'uno.css'
 import * as Pinia from 'pinia'
+import { createSSRApp } from 'vue'
 import App from './App.vue'
+import 'uno.css'
 
 export function createApp() {
   const app = createSSRApp(App)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
 import Components from '@uni-helper/vite-plugin-uni-components'
 import { WotResolver } from '@uni-helper/vite-plugin-uni-components/resolvers'
-import UniPages from '@uni-helper/vite-plugin-uni-pages'
 import UniLayouts from '@uni-helper/vite-plugin-uni-layouts'
 import UniManifest from '@uni-helper/vite-plugin-uni-manifest'
-import uni from '@dcloudio/vite-plugin-uni'
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig(async () => {
   const UnoCSS = (await import('unocss/vite')).default
@@ -14,33 +15,29 @@ export default defineConfig(async () => {
   return {
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
     plugins: [
       Components({
-      dts: true,
-      resolvers: [WotResolver()]
-    }),
+        dts: true,
+        resolvers: [WotResolver()],
+        directoryAsNamespace: true,
+      }),
       // https://github.com/uni-helper/vite-plugin-uni-pages
-    UniPages(),
+      UniPages(),
       // https://github.com/uni-helper/vite-plugin-uni-layouts
-    UniLayouts(),
+      UniLayouts(),
       // https://github.com/uni-helper/vite-plugin-uni-manifest
-    UniManifest(),
+      UniManifest(),
       uni(),
+      AutoImport({
+        imports: ['vue', '@vueuse/core', 'uni-app'],
+        dts: 'src/auto-imports.d.ts',
+        dirs: ['src/composables', 'src/stores', 'src/utils'],
+        vueTemplate: true,
+      }),
       UnoCSS(),
     ],
-    build: {
-    target: 'es6',
-    cssTarget: 'chrome61',
-  },
-  optimizeDeps: {
-    exclude: [
-      'vue-demi',
-    ],
-  },  }
+  }
 })
-
-
-
